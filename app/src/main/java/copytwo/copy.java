8e93fbe3410a49e8d5d3a3109e879cd1ae430d55
panzhanguo <1281923219@qq.com>
2018-11-24 18:11:15 +0800

    预编译4

--- app/src/main/java/copytwo/copy.java
+++ app/src/main/java/copytwo/copy.java
@@ -1,91 +1,45 @@
 package copytwo;
 
-import android.support.v4.widget.TextViewCompat;
-import android.util.Log;
 
-import net.sourceforge.jtds.jdbc.JtdsConnection;
+import android.util.Log;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
-import java.util.ArrayList;
-import java.util.List;
 
 public class copy {
-    List<Student> li;
-
-
+    /**
+     * SQLife
+     */
     public  void  jdbc() {
 
-        li = new ArrayList<Student>();
+        try{
+            Class.forName("org.sqlite.JDBC");
+           String url="jdbc:sqlite:test.db";
+         Connection con = DriverManager.getConnection(url);
+         String  sql="select * from 表.test";
+          Statement sta=con.createStatement();
+          ResultSet rs = sta.executeQuery(sql);
 
-        try {
-            String sql2 = "select * from guest.Student";
 
-            String connectionUrl = "jdbc:jtds:sqlserver://192.168.165.180;databaseName=PZG;user=TechCent_PZG;password=12345678";
-            try (Connection con = DriverManager.getConnection(connectionUrl); Statement stmt = con.createStatement();) {
 
-                try (ResultSet rs = stmt.executeQuery(sql2)) {
+          while (rs.next()) {
+                int id = rs.getInt("id");
+                String word= rs.getString("word");
+                String detail = rs.getString("detail");
+                Log.e("gong",id+word+detail);
 
-                    while (rs.next()) {
-                        int age = rs.getInt("age");
-                        String name = rs.getString("name");
-                        Student st = new Student();
-                        st.setAge(age);
-                        st.setName(name);
-                        li.add(st);
 
-                    }
-                }
-            } catch (SQLException e) {
-                e.printStackTrace();
+
             }
+
 
         } catch (Exception e) {
-
             e.printStackTrace();
-            System.out.print("出现异常");
-
-
-        }
-    }
-    public  void test(){
-
-        for (int i=0;i<li.size();i++){
-            int age2=  li.get(i).getAge();
-            String  name2=li.get(i).getName();
-
-            Log.e("gong",name2+age2);
-
-           String connectionUrl = "jdbc:jtds:sqlserver://192.168.165.180;databaseName=PZG;user=TechCent_PZG;password=12345678";
-
-                try {
-                    String sql="insert into guest.Teacher(age, name) values(#{age2},#{name2})";
-                    Connection con = DriverManager.getConnection(connectionUrl); PreparedStatement stmt = con.prepareStatement(sql);
-                    stmt.setInt( 1,age2);
-                    stmt.setString(2,name2);
-
-
-
-             int oo= stmt.executeUpdate();
-
-
-
-
-
-            }
-
-
-            catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-
-li.clear();
-
+          }
+       }
 
     }
 
@@ -95,5 +49,4 @@ li.clear();
 
 
 
-}
 
